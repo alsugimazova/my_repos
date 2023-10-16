5a1f46abd7751cf9900651fb55fb40fa984498a3
alsugimazova <gimasova.alsu@gmail.com>
2023-10-16 12:18:56 +0300

    генерация продуктов реакции Сузуки

--- suzuki/generation_of_reactions.py
+++ suzuki/generation_of_reactions.py
@@ -1,24 +1,26 @@
 from chython import smiles
 from chython.reactor import reactions
-reagents_path = 'example.txt'
-product = []
-counter = 0
-with open(reagents_path) as f:
-    list_of_molecules = f.readlines()
-for mol in list_of_molecules:
-    mol = smiles(mol)
-    reagent_1 = mol
-    counter += 1
-    for molec in list_of_molecules[counter:]:
-        molec  = smiles(molec)
-        reagent_2 = molec
-        try:
-            reaсtion = next(reactions.suzuki(reagent_1,reagent_2))
-        except:
-            continue
-        for molecule in reaсtion.products:
-            molecule = str(molecule)
-            product.append(molecule)
-with open('product.txt','w') as f:
-    for mol in product:
-        f.write(f'{mol}\n')
+from itertools import combinations
+from tqdm import tqdm
+
+def read(reagents_path):
+    with open(reagents_path) as f:
+        list_of_molecules = [line.rstrip() for line in f]
+    return(list_of_molecules)
+
+def run():
+    dataset = read('zinc.smi')
+    path = 'product_zinc.txt'
+    with open(path,'w') as f:
+        for mols_pair in tqdm(combinations(dataset, 2)):
+            reagent_1 = mols_pair[0]
+            reagent_1 = smiles(reagent_1)
+            reagent_1 .canonicalize()
+            reagent_2 = mols_pair[1]
+            reagent_2 = smiles(reagent_2)
+            reagent_2 .canonicalize()
+            for reaction in reactions.suzuki(reagent_1, reagent_2):
+                f.write(f'{str(reaction.products[0])}\n')
+
+if __name__ == '__main__':
+    run()
