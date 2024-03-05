db087cc4db8fb0ffe4283f53cb7fe60c990ace52
alsugimazova <gimasova.alsu@gmail.com>
2024-03-06 00:10:17 +0300

    updating reaction generator and predict.py

--- src/generator/b_h_reaction.py
+++ src/generator/b_h_reaction.py
@@ -6,25 +6,29 @@ from rdkit import Chem
 
 def read(reagents_path):
     with open(reagents_path) as f:
-        list_of_molecules = [line.rstrip() for line in f][49:]
+        list_of_molecules = [line.rstrip() for line in f]
     return(list_of_molecules)
 
 def run(input_path, output_path):
     dataset = read(input_path)
     with open(output_path,'w') as f:
         for mols_pair in tqdm(combinations(dataset, 2)):
+            rct = str()
             reagent_1 = mols_pair[0]
             reagent_1 = Chem.MolToSmiles(Chem.MolFromSmiles(reagent_1))
             reagent_1 = smiles(reagent_1)
+            rct += str(reagent_1) + '.'
             reagent_1.canonicalize()
             reagent_2 = mols_pair[1]
             reagent_2 = Chem.MolToSmiles(Chem.MolFromSmiles(reagent_2))
             reagent_2 = smiles(reagent_2)
+            rct += str(reagent_2) + '>>'
             reagent_2.canonicalize()
             for reaction in reactions.buchwald_hartwig(reagent_1, reagent_2):
-                f.write(f'{str(reaction.products[0])}\n')
+                rct += str(reaction.products[0])
+                f.write(f'{rct}\n')
                 break
 
 if __name__ == '__main__':
-    run('input_path.txt', 'b_h_products.txt')
-    
+    run('src/generator/test.txt', 'src/generator/BH_pool.txt')
+
